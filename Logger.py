153d28dc59d1af525aa07b67beaa821e1a9bc092
Jonator3 <joey.pehlke@gmail.com>
2020-11-28 08:34:51 +0100

    just some testing left

--- Logger.py
+++ Logger.py
@@ -1,16 +1,21 @@
 from datetime import datetime
 import os
 
+LogFile = None
 
-time = datetime.now()
-os.mkdir("Logs")
-os.mkdir(time.strftime("Logs/%b-%y"))
-LogFile = open(time.strftime("Logs/%b-%y/%d-%a.log"), "w+")
+
+def start():
+    global LogFile
+    time = datetime.now()
+    os.mkdir("Logs")
+    os.mkdir(time.strftime("Logs/%b-%y"))
+    LogFile = open(time.strftime("Logs/%b-%y/%d-%a.log"), "w+")
 
 
 def logged_print(*args):
     s = datetime.now().strftime("%X| ")
     for arg in args:
         s += str(arg)
-    LogFile.write(s)
+    if LogFile:
+        LogFile.write(s)
     print(s)
